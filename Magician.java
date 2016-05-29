4806b0ea0a78fc367d53edea78423471a5a1b98d
Fanfan-Shen <1217485070@qq.com>
2016-05-29 17:31:51 +0800

    Role Playing Game

---
+++ Magician.java
@@ -0,0 +1,25 @@
+package cc.openhome;
+
+
+public class Magician extends Role{
+
+
+    public void fight(){
+
+
+     System.out.println("Ä§·¨¹¥»÷");
+
+
+     }
+
+
+   public void cure()  {
+
+
+      System.out.println("Ä§·¨ÖÎÁÆ");
+
+    }
+
+
+
+}
---
+++ SwordsMan.java
@@ -0,0 +1,21 @@
+package cc.openhome;
+
+
+public class SwordsMan extends Role{
+
+    public void fight(){
+
+
+     System.out.println("»Ó½£¹¥»÷");
+
+
+
+      }
+
+
+
+
+
+
+
+}
